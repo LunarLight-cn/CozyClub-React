3e0cbcd27e4931c9aa36e2b9eaa5fb06df0db87f
LunarLight <lunarlight.cn@gmail.com>
2025-11-27 01:16:39 +0700

    Rebase

--- src/components/playground/GamesMain.jsx
+++ src/components/playground/GamesMain.jsx
@@ -5,23 +5,7 @@ import './styleGamesMain.css';
 
 const GamesMain = () => {
   return (
-    <div className="min-h-screen p-10 flex flex-col items-center">
-      <h1 className="text-4xl font-bold text-(--primary-color) mb-10">
-        Playground
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <Link to="/games/insider" className="game-card block p-6 bg-(--sunlit-clay-100) rounded-xl shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-300">
-          <h2 className="text-2xl font-bold text-(--primary-color) mb-2">Insider (TH)</h2>
-          <p className="text-(--dark-color)">Insider (Th)</p>
-          <div className="mt-4 px-4 py-2 bg-(--secondary-color) text-white rounded-full text-center inline-block">
-            Play Now
-          </div>
-        </Link>
-
-        {/* for more Games */}
-
-      </div>
-    </div>
+    <div></div>
   )
 }
 
